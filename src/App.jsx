220613e2d3d8106b15eba1c697e7b10fad9bc991
rootnure <nur.diu.2791@gmail.com>
2023-09-08 08:17:09 +0600

    Different way to handle events in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  function handleClick() {
+    alert('btn clicked');
+  }
+
+  const handleClick2 = () => {
+    alert('btn2 clicked');
+  }
+
+  const addFive = (num) => {
+    alert(num + 5);
+  }
+
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      {/* <button onclick="handleClick()">Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me 2</button>
+      {/* Vejailla */}
+      <button onClick={() => alert('Click3 Clicked')}>Click 3</button>
+      <button onClick={() => addFive(3)}>Add Five</button>
     </>
   )
 }
