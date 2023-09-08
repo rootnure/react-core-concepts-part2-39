bed1fde977c6f66e94c3da23bc9a3558bb6c49a4
rootnure <nur.diu.2791@gmail.com>
2023-09-08 07:40:29 +0600

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+    </>
+  )
+}
+
+export default App
